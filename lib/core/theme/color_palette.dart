133cf935b0aeec72d68bbdf0e056864a38e8561f
Motaz <motazyusuf@gmail.com>
2024-09-03 02:50:49 +0300

    init

---
+++ lib/core/theme/color_palette.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorPaletteDark{
+  Color primaryColor = const Color(0xFF0F0F0F);
+  Color accentColor = const Color(0xfffe979a);
+  Color secondaryPrimaryColor = const Color(0xffa11b25);
+  Color secondaryAccentColor = const Color(0xFFFFD600);
+  Color bodyTextColor = Colors.white;
+
+}
